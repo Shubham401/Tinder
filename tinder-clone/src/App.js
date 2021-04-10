e298cae82dadd6d07d047fbad02b66afea03d394
Shubham401 <shubham26.bdps@gmail.com>
2021-04-10 16:57:52 +0530

    Header Component

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Tinder clone</h1>
+    <div className="app">
+      <Header />
+      {/* TinderCards */}
+      {/* SwipeButtons */}
     </div>
   );
 }
